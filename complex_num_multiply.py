59d5d1a135c927a61a58788a38970a044a7893ab
WilliamBasquez <43230765+WilliamBasquez@users.noreply.github.com>
2018-12-06 14:53:03 -0700

    Add files via upload

---
+++ complex_num_multiply.py
@@ -0,0 +1,11 @@
+#create a list inside store the values of the multiplications
+#at the end 
+def complexNumberMultiply(a, b):
+	answer = []
+	for i in range(len(a)):
+	#[ ][1][+][i]
+	#[1][1][+][i]
+	#[+][+][+][i]
+	#[i][i][i][i^2]
+
+	1 + i^2 + 2 * i
